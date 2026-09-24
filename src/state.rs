3f205010581737161e393f6ff9d2995265868c8e
myselfBZ <boburforfun@gmail.com>
2026-09-24 08:59:34 +0500

    feat: clean up

--- src/state.rs
+++ src/state.rs
@@ -1,17 +1,14 @@
-use std::collections::HashMap;
-use crate::entities::User;
 use sqlx::PgPool;
 
 use crate::db;
 
 pub struct MyState {
     pub db: PgPool,
-    pub users: HashMap<String, User>
 }
 
 impl MyState {
     pub async  fn new(pg_url: &str) -> Self {
         let conn = db::open_db_connection(pg_url).await.expect("failed to connect to database");
-        return Self{db: conn, users: HashMap::new()}
+        return Self{db: conn}
     }
 }
