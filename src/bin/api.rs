fa1c64bee11e3e1d207da88b4460b34852e6ff6a
myselfBZ <boburforfun@gmail.com>
2026-09-24 09:03:08 +0500

    feat: loading db url from env

--- src/bin/api.rs
+++ src/bin/api.rs
@@ -3,10 +3,13 @@ use axum::{Router, routing::{delete, get, post}};
 use basic_api::state::MyState;
 use basic_api::handlers;
 use basic_api::middleware;
+use dotenvy::dotenv;
 
 #[tokio::main]
 async fn main() {
-    let app_state = MyState::new("postgresql://postgres:pass@localhost:5432/basic_api?sslmode=disable").await;
+    dotenv().ok();
+    let db_url = std::env::var("DATABASE_URL").expect("where's the database?");
+    let app_state = MyState::new(&db_url).await;
     let shared = Arc::new(app_state);
     let app = Router::new()
         .route("/users/{id}", get(handlers::get_user))
